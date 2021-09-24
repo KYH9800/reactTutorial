99bd12c751bf5d204a2454a6b5122eb840c7fed0
고윤혁 <sksgur3217@gmail.com>
2021-09-24 17:38:43 +0900

    complete refactoring hooks (es2021JS/09/24)

--- 06-rockScissorPaper/App.jsx
+++ 06-rockScissorPaper/App.jsx
@@ -1,12 +1,12 @@
 // es2015 module 문법 (babel이 import도 requier로 바꿔준다)
 import React from "react";
-// import RSPHooks from "./components-hooks/RSPHooks";
-import RSPClass from "./components-class/RSPClass";
+import RSPHooks from "./components-hooks/RSPHooks";
+// import RSPClass from "./components-class/RSPClass";
 
 //* 반응속도체크
 const App = () => {
-  // return <RSPHooks />;
-  return <RSPClass />;
+  return <RSPHooks />;
+  // return <RSPClass />;
 };
 
 export default App;
---
+++ 06-rockScissorPaper/components-hooks/RSPHooks.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./RSPHooks.css";
+//! hooks는 lifecycle이 없지만 흉내는 낼 수 있다
+// 좌표
+const rspCoords = {
+  rock: "0",
+  scissor: "-142px",
+  paper: "-284px",
+};
+
+const scores = {
+  scissor: 1,
+  rock: 0,
+  paper: -1,
+};
+
+const computerChoice = (imgCoord) => {
+  return Object.entries(rspCoords).find(function (v) {
+    return v[1] === imgCoord;
+  })[0];
+};
+
+const RSP = () => {
+  const [result, setResult] = useState("");
+  const [imgCoord, setImgCoord] = useState(rspCoords.rock);
+  const [score, setScore] = useState(0);
+  const interval = useRef();
+
+  useEffect(() => {
+    // console.log("다시 실행"); componentDidMount()
+    interval.current = setInterval(changeHand, 100);
+    return () => {
+      // console.log("종료"); componentWillUnmount()
+      clearInterval(interval.current);
+    };
+  }, [imgCoord]);
+
+  const changeHand = () => {
+    if (imgCoord === rspCoords.rock) {
+      setImgCoord(rspCoords.scissor);
+    } else if (imgCoord === rspCoords.scissor) {
+      setImgCoord(rspCoords.paper);
+    } else if (imgCoord === rspCoords.paper) {
+      setImgCoord(rspCoords.rock);
+    }
+  };
+
+  const onClickButton = (choise) => () => {
+    if (interval.current) {
+      clearInterval(interval.current);
+      interval.current = null;
+    }
+    const myScore = scores[choise];
+    const cpuScore = scores[computerChoice(imgCoord)];
+    const diff = myScore - cpuScore;
+    if (diff === 0) {
+      setResult("비겼습니다!");
+    } else if ([-1, 2].includes(diff)) {
+      setResult("이겼습니다!");
+      setScore((prevScore) => prevScore + 1);
+    } else {
+      setResult("졌습니다!");
+      setScore((prevScore) => prevScore - 1);
+    }
+    setTimeout(() => {
+      interval.current = setInterval(changeHand, 100);
+    }, 2000);
+  };
+
+  return (
+    <>
+      <div id="computer" style={{ background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0` }} />
+      <div>
+        <button id="scissor" className="btn" onClick={onClickButton("scissor")}>
+          가위
+        </button>
+        <button id="rock" className="btn" onClick={onClickButton("rock")}>
+          바위
+        </button>
+        <button id="paper" className="btn" onClick={onClickButton("paper")}>
+          보
+        </button>
+      </div>
+      <div>{result}</div>
+      <div>현재 {score} 점</div>
+    </>
+  );
+};
+
+export default RSP;
