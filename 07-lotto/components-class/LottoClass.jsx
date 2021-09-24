c84ca8aedbf4463a1ba334fee7d9e5d94fa7f043
고윤혁 <sksgur3217@gmail.com>
2021-09-24 18:16:40 +0900

    start lotto-lottery (es2021JS/09/24)

---
+++ 07-lotto/components-class/LottoClass.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class LottoClass extends Component {
+  render() {
+    return (
+      <>
+        <div>LottoClass</div>
+      </>
+    );
+  }
+}
+
+export default LottoClass;
---
+++ 07-lotto/components-hooks/LottoHooks.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LottoHooks = () => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+
+export default LottoHooks;
