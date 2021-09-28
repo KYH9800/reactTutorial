98347a906e4b3bd56cd0182ec851f494f09b84cb
고윤혁 <sksgur3217@gmail.com>
2021-09-29 00:38:46 +0900

    start react-router (es2021JS/09/29)

---
+++ 10-React-Router/App.jsx
@@ -0,0 +1,9 @@
+// es2015 module 문법 (babel이 import도 requier로 바꿔준다)
+import React from "react";
+import ReactRouter from "./components/ReactRouter";
+
+const App = () => {
+  return <ReactRouter />;
+};
+
+export default App;
---
+++ 10-React-Router/components/ReactRouter.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ReactRouter = () => {
+  // todo
+  return (
+    <>
+      <div>Router</div>
+    </>
+  );
+};
+
+export default ReactRouter;
