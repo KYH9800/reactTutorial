4d26fb833a08e47eab68a979f1fc1cfdf19d3853
고윤혁 <sksgur3217@gmail.com>
2021-09-30 23:54:51 +0900

    BrowserRouter, HashRouter, params, withRouter, location, history, match, queryString

---
+++ 10-React-Router/components/GameMatcher.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+import WordPlay from "../../02-wordRelay-react/App";
+import ResponseCheckClass from "../../05-responseCheck/components-class/ResponseCheckClass";
+import RSPClass from "../../06-rockScissorPaper/components-class/RSPClass";
+import LottoClass from "../../07-lotto/components-class/LottoClass";
+
+export default class GameMatcher extends Component {
+  render() {
+    console.log("history: ", this.props.history, "location: ", this.props.location, "match: ", this.props.match);
+    console.log("location.search: ", this.props.location.search);
+    const { match } = this.props;
+    if (match.params.name === "끝말잇기") {
+      return <WordPlay />;
+    } else if (match.params.name === "반응속도체크") {
+      return <ResponseCheckClass />;
+    } else if (match.params.name === "가위바위보") {
+      return <RSPClass />;
+    } else if (match.params.name === "로또추첨기") {
+      return <LottoClass />;
+    }
+    return (
+      <>
+        <div>일치하는 게임이 없습니다.</div>
+      </>
+    );
+  }
+}
+
+// this.props.history: 앞으로가기 뒤로가기 기록이 있다 (react router 눈속임을 위한 정보)
+// this.props.location: 주소만 보고 주소에 대한 정보
+// this.props.match: params애 대한 정보
--- 10-React-Router/components/ReactRouter.jsx
+++ 10-React-Router/components/ReactRouter.jsx
@@ -1,31 +1,27 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import "./ReactRouter.css";
-import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
-
-import WordPlay from "../../02-wordRelay-react/App";
-import ResponseCheckClass from "../../05-responseCheck/components-class/ResponseCheckClass";
-import RSPClass from "../../06-rockScissorPaper/components-class/RSPClass";
-import LottoClass from "../../07-lotto/components-class/LottoClass";
+import { HashRouter, Route, Link, Switch } from "react-router-dom";
+// GameMatcher
+import GameMatcher from "./GameMatcher";
 
 const ReactRouter = () => {
-  // todo
   return useMemo(() => (
-    <BrowserRouter>
+    // QueryString: /game/:name?key=value&key=value&key=value
+    // 사용자가 입력 데이터를 전달하는 방법중의 하나로, url 주소에 미리 협의된 데이터를 파라미터를 통해 넘기는 것을 말한다.
+    <HashRouter>
       <div className="var">
-        <Link to="/끝말잇기">끝말잇기</Link>
-        <Link to="/반응속도체크">반응속도체크</Link>
-        <Link to="/가위바위보">가위바위보</Link>
-        <Link to="/로또추첨기">로또추첨기</Link>
+        <Link to="/game">처음화면</Link>
+        <Link to="/game/끝말잇기?query=10&hello=kyh&by=react-router">끝말잇기</Link>
+        <Link to="/game/반응속도체크?query=11&hello=kyh&by=react-router">반응속도체크</Link>
+        <Link to="/game/가위바위보?query=12&hello=kyh&by=react-router">가위바위보</Link>
+        <Link to="/game/로또추첨기?query=13&hello=kyh&by=react-router">로또추첨기</Link>
       </div>
       <div className="main">
         <Switch>
-          <Route path="/끝말잇기" component={WordPlay} />
-          <Route path="/반응속도체크" component={ResponseCheckClass} />
-          <Route path="/가위바위보" component={RSPClass} />
-          <Route path="/로또추첨기" component={LottoClass} />
+          <Route path="/game/:name" component={GameMatcher} />
         </Switch>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   ));
 };
 
@@ -34,19 +30,8 @@ export default ReactRouter;
 // npm install react-router
 // npm install react-router-dom
 
-/*
-<BrowserRouter>
-      <Link to="/">Home</Link>
-      <Link to="/GuGuDan">구구단</Link>
-      <Link to="./">wordRelay</Link>
-      <Link to="./">numberBaseball</Link>
-      <Link to="./">responseCheck</Link>
-      <Link to="./">RSP</Link>
-      <Link to="./">Lotto</Link>
-      <Link to="./">MineGame</Link>
-      <Link to="./"></Link>
-      <div>
-        <Router path="/GuGuDan" component={GuGuDan} />
-      </div>
-    </BrowserRouter>
-*/
+/* HashRouter: 프론트엔드만 알수있다. (서버쪽으로 요청이가서 새로고침하면 Cannot GET / message 가 뜨면서 서버쪽에선 알 수가 없다) */
+/* HashRouter: 프론트엔드에 요청이간다. server는 모르고 browser만 안다.
+- "#" 뒤에 부분은 브라우저만 아는 부분이다 (react router도 안다)
+- 단점: server가 모르기 때문에 검색엔진에는 문제가 된다
+- 실무에서는 HashRouter를 쓴다 */
